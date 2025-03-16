e28175353727cd57b3cafb9ca0f175293927480e
BenH9999 <ben.houghton7070outlook.com>
2025-03-16 09:21:36 +0000

    add cv

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,5 +1,3 @@
-
-
 export const Home = () => {
   return (
     <section id="home" className="min-h-screen flex items-center justify-center relative">
@@ -20,7 +18,7 @@ export const Home = () => {
           <a href="#contact" className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.2)] hover:bg-blue-500/10">
             Contact Me
           </a>
-          <a href="/mycv.pdf" rel="noopener noreferrer" className="bg-gradient-to-r from-blue-400 to-purple-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(128,90,213,0.4)]">
+          <a href="/public/Benjamin_Houghton_CV.pdf" rel="noopener noreferrer" className="bg-gradient-to-r from-blue-400 to-purple-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(128,90,213,0.4)]">
             View CV
           </a>
         </div>
