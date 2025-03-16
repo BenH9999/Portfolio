6d654c3a9bb5d7eb1d5009f49d3bbbd414ef0109
BenH9999 <ben.houghton7070outlook.com>
2025-03-16 21:26:23 +0000

    GEMINI ANIMATION

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+"use client";
+import { useScroll, useTransform } from "motion/react";
 import { useState } from "react";
 import './App.css';
 import { LoadingScreen } from './components/LoadingScreen';
@@ -8,16 +10,36 @@ import { About } from "./components/sections/About";
 import { Projects } from "./components/sections/Projects";
 import { Contact } from "./components/sections/Contact";
 import { FadeSection } from "./components/FadeSection";
+import { GeminiEffect } from "./components/GeminiEffect";
 import './index.css';
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
 
+  const { scrollYProgress } = useScroll();
+
+  const pathLengthFirst = useTransform(scrollYProgress, [0, 1], [0, 1]);
+  const pathLengthSecond = useTransform(scrollYProgress, [0, 1], [0, 1]);
+  const pathLengthThird = useTransform(scrollYProgress, [0, 1], [0, 1]);
+  const pathLengthFourth = useTransform(scrollYProgress, [0, 1], [0, 1]);
+  const pathLengthFifth = useTransform(scrollYProgress, [0, 1], [0, 1]);
+
   return (
     <>
       {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
-      <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
+      <div className={`relative min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
+      <div className="fixed top-1/2 left-0 w-full h-[445px] -translate-y-1/2 pointer-events-none opacity-20 z-0">
+        <GeminiEffect
+          pathLengths={[
+          pathLengthFirst,
+          pathLengthSecond,
+          pathLengthThird,
+          pathLengthFourth,
+          pathLengthFifth,
+          ]}
+        />
+      </div>
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <FadeSection rootMargin="0px 0px -100px 0px">
