e785d2629b3e3feb359114e4c886932bb713d93a
BenH9999 <ben.houghton7070outlook.com>
2025-03-17 12:07:08 +0000

    more skills

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -3,7 +3,7 @@
 export const About = () => {
 
   const frontendSkills = ["React", "React Native", "JavaScript", "TypeScript", "TailwindCSS"];
-  const backendSkills = ["Golang", "C++", "Java", "Python", "AWS", "Postgres", "MySQL"];
+  const backendSkills = ["Golang", "C++", "Java", "C#", "Python", "AWS", "Postgres", "MySQL"];
   const tools = ["Git", "npm", "CMake", "Visual Studio", "Linux", "Neovim"];
 
   return (
